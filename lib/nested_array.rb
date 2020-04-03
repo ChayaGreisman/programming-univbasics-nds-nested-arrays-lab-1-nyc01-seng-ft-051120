cc9966888937585423909d6bb45faa3a44cd47b1
Chaya Greisman <chayamgreisman@gmail.com>
2020-04-03 22:09:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -64,6 +64,7 @@ def matrix_lookup(matrix, row, column)
   # Return the matrix's content at that row and and 
   
   matrix[row][column]
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
